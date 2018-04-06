6a6a29f09b969b187df2278e4bf5a1e9fb578042
Tom Delaney <tom.delaney@8451.com>
2018-04-06 11:39:30 -0400

    modifying the entire app from single script

--- README.md
+++ README.md
@@ -1,22 +1,5 @@
 # Adding clarity to your fresh Angular-cli project
 
-1) copy add-clarity.sh to the root of your new cli project  
-2) copy app.component.html to the root of your new cli project
-3) Import the ClarityModule into your Angular application's module. Your application's main module might look like this:  
-```
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { ClarityModule } from "@clr/angular";
-import { AppComponent } from "./app.component";
-
-@NgModule({
-    imports: [
-        BrowserModule,
-        ClarityModule,
-        ...
-     ],
-     declarations: [ AppComponent ],
-     bootstrap: [ AppComponent ]
-})
-export class AppModule {    }
-```
+1) copy add-clarity.sh to the root of your new cli project
+2) chmod + x add-clarity.sh
+3) ./add-clarity.sh
--- add-clarity.sh
+++ add-clarity.sh
@@ -1,37 +1,68 @@
 #!/bin/bash
 
-# YARN add clarity dependencies
+# Add Clarity dependencies to YARN
+YARN add clarity dependencies
 yarn add @clr/icons
 yarn add @webcomponents/custom-elements@1.0.0
 yarn add @clr/ui
 yarn add @clr/angular
 
 # Clarity Scripts for .angular-cli.json
-SCRIPTS_LINE_1="      \"scripts\": [?"
-SCRIPTS_LINE_2="        \"..\/node_modules\/@clr\/icons\/clr-icons.min.css\",?"
-SCRIPTS_LINE_3="        \"..\/node_modules\/@webcomponents\/custom-elements\/custom-elements.min.js\",\?"
-SCRIPTS_LINE_4="        \"..\/node_modules\/@clr\/icons\/clr-icons.min.js\"?"
-SCRIPTS_LINE_5="      ],"
+SCRIPTS_LINE="      \"scripts\": [?"
+SCRIPTS_LINE="$SCRIPTS_LINE        \"..\/node_modules\/@clr\/icons\/clr-icons.min.css\",?"
+SCRIPTS_LINE="$SCRIPTS_LINE        \"..\/node_modules\/@webcomponents\/custom-elements\/custom-elements.min.js\",\?"
+SCRIPTS_LINE="$SCRIPTS_LINE        \"..\/node_modules\/@clr\/icons\/clr-icons.min.js\"?"
+SCRIPTS_LINE="$SCRIPTS_LINE      ],"
 
-SCRIPTS_LINE="$SCRIPTS_LINE_1$SCRIPTS_LINE_2$SCRIPTS_LINE_3$SCRIPTS_LINE_4$SCRIPTS_LINE_5"
+# Clarity css for .angular-cli.json
+CSS_LINE="      \"styles\": [?"
+CSS_LINE="$CSS_LINE        \"..\/node_modules\/@clr\/icons\/clr-icons.min.css\",?"
+CSS_LINE="$CSS_LINE        \"..\/node_modules\/@clr\/ui\/clr-ui.min.css\","
 
-CSS_LINE_1="      \"styles\": [?"
-CSS_LINE_2="        \"..\/node_modules\/@clr\/icons\/clr-icons.min.css\",?"
-CSS_LINE_3="        \"..\/node_modules\/@clr\/ui\/clr-ui.min.css\","
-
-CSS_LINE="$CSS_LINE_1$CSS_LINE_2$CSS_LINE_3"
-
+# Backup the files we're manipulating
 cp .angular-cli.json .angular-cli.json_bak
+cp src/app/app.component.html src/app/app.component.html_bak
+
+# Remove the . from .angular-cli.json while we work on it
 cp .angular-cli.json angular-cli.json
+
+# Add the scripts to .angular-cli.json
 sed "24s/.*/${SCRIPTS_LINE}/" angular-cli.json > new_angular-cli.json
 tr ? '\n' < new_angular-cli.json > angular-cli.json
 
+# Add the css to .angular-cli.json
 sed "21s/.*/${CSS_LINE}/" angular-cli.json > new_angular-cli.json
 tr ? '\n' < new_angular-cli.json > angular-cli.json
 
-rm new_angular-cli.json
-
+# add the . back into .angular-cli.json
 mv angular-cli.json .angular-cli.json
 
-cat app.component.html > src/app/app.component.html
-rm app.component.html
+# New app.component.html template making use of clarity classes
+APP_COMPONENT_LINE="<div class=\"main-container\">"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?    <div class=\"alert alert-app-level\">?"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?    </div>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?    <header class=\"header header-6\">"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?        <div class=\"branding\">"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?            <span class=\"title\">{{ title }}</span>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?        </div>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?    </header>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?    <div class=\"content-container\">"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?        <div class=\"content-area\">"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?        <p>Your content will go here</p>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?        </div>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?    </div>"
+APP_COMPONENT_LINE="$APP_COMPONENT_LINE?</div>"
+
+# Overwrite out app.component.html with the new contents
+echo $APP_COMPONENT_LINE | tr ? '\n' > src/app/app.component.html
+
+
+IMPORTS_LINES="    BrowserModule,?    ClarityModule"
+sed "13s/.*/${IMPORTS_LINES}/" src/app/app.module.ts | tr ? '\n' > src/app/new_app.module.ts
+
+IMPORT_LINE="import { ClarityModule } from '@clr\/angular';"
+sed "3s/.*/${IMPORT_LINE}/" src/app/new_app.module.ts > src/app/app.module.ts
+
+# Cleanup our mess
+rm new_angular-cli.json
+rm src/app/new_app.module.ts
